fb96375077cec968ff5eca1559a0bcc4cf87bc3f
Serhat Teker <serhat.teker@gmail.com>
2020-09-16 18:02:22 +0300

    Add vone views

--- todo/vone/views.py
+++ todo/vone/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# from rest_framework.views import APIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
 
-# Create your views here.
+from .models import Category, Tag, Task
+from .serializers import TaskSerializer
+
+
+class TodoListView(ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TodoDetailView(RetrieveUpdateDestroyAPIView):
+    lookup_field = 'id'
+    queryset = Task.objects.all()
+    # permission_classes = (permissions.AllowAny,)
+    serializer_class = TaskSerializer
