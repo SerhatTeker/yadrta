3598cb3be8bfac6a0d4e2cf8a2746545de7601c4
Serhat Teker <serhat.teker@gmail.com>
2020-09-23 17:47:19 +0300

    Delete old todo project

--- todo/vone/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Tag, Category, Task
-
-admin.site.register(Tag)
-admin.site.register(Category)
-admin.site.register(Task)
--- todo/vone/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class VoneConfig(AppConfig):
-    name = 'todo.vone'
