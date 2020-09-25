fc3262f34107eaac8393ad8a34a0eb0a066b330a
Serhat Teker <serhat.teker@gmail.com>
2020-09-25 19:25:08 +0300

    Add CategoryFactory

--- tests/vone/factories.py
+++ tests/vone/factories.py
@@ -1,7 +1,19 @@
 import factory
 
-from src.vone.models import Tag
+from src.vone.models import Category, Tag
 from tests.users.factories import UserFactory
+
+
+class CategoryFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Category
+        django_get_or_create = ("name",)
+
+    id = factory.Faker("pyint")
+    name = factory.Faker("word")
+    created_by = factory.SubFactory(UserFactory)
+    created_at = factory.Faker("date")
+    changed_at = factory.Faker("date")
 
 
 class TagFactory(factory.django.DjangoModelFactory):
