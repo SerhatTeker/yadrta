e4b739341c48b28ae3839b9cbdf0407a8a43d510
Serhat Teker <serhat.teker@gmail.com>
2020-09-28 17:18:41 +0300

    Add tag detail tests
    DELETE and PUT

--- tests/vone/test_tag_views.py
+++ tests/vone/test_tag_views.py
@@ -1,15 +1,20 @@
+import logging
+
 from django.urls import reverse
 from faker import Faker
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+from src.vone.models import Tag
+
 from .factories import TagFactory
 from .utils import user_id_to_hex
 
 fake = Faker()
+LOGGER = logging.getLogger(__name__)
 
 
-class TestTagListView(APITestCase):
+class TestTagListAPIView(APITestCase):
     """
     Tests /tag list operations.
     """
@@ -54,31 +59,55 @@ class TestTagCreateAPIView(APITestCase):
         self.assertEqual(status.HTTP_201_CREATED, response.status_code)
 
 
-class TestTagDetailView(APITestCase):
+class TestTagDetailAPIView(APITestCase):
     """
     Tests /tag detail operations.
     """
 
     def setUp(self):
+        # Tag
         self.url = reverse("tag-list")
         self.tag = TagFactory()
-        print(f"tag: {self.tag}")
-        print(f"created_by: {self.tag.created_by}")
+        self.url_detail = reverse("tag-detail", kwargs={"pk": self.tag.pk})
+        # User
         self.user = self.tag.created_by
         self.api_authentication()
+        # API
+        self.payload = self.set_payload()
         self.response = self.set_response()
 
     def api_authentication(self):
         self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.user.auth_token}")
 
-    def set_response(self):
-        payload = {"name": self.tag.name, "created_by": self.user.pk}
-        return self.client.post(self.url, payload)
+    def set_payload(self):
+        return {"name": self.tag.name, "created_by": self.user.pk}
 
-    def test_get_request_returns_a_given_tag(self):
+    def _set_response(self, url, payload=None, name=None, created_by=None):
+        if not payload:
+            payload = {"name": name, "created_by": created_by}
+
+        return self.client.post(url, payload)
+
+    def set_response(self):
+        """Default response for cls"""
+        response = self._set_response(url=self.url, payload=self.payload)
+        return response
+
+    def test_get_request_returns_a_givzen_tag(self):
         self.assertEqual(self.response.data.get("name"), self.tag.name)
 
     def test_get_request_returns_user_id(self):
         self.assertEqual(
             self.response.data.get("created_by").hex, user_id_to_hex(self.user.id)
         )
+
+    def test_tag_object_update(self):
+        response = self.client.put(self.url_detail, self.payload)
+        tag = Tag.objects.get(id=self.tag.id)
+        self.assertEqual(response.data.get("name"), tag.name)
+
+    def test_tag_object_delete(self):
+        # LOGGER.info(f"response before delete: {self.response}")
+        response = self.client.delete(self.url_detail)
+        # LOGGER.info(f"response after delete: {response}")
+        self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
--- tests/vone/utils.py
+++ tests/vone/utils.py
@@ -1,10 +1,15 @@
 from uuid import UUID
 
+
 def user_id_to_hex(uuid: str) -> str:
-    """
-    Turn uuid4 with hashes to hex:
+    """Turn uuid4 with dashes to hex:
     From    :  8791f25b-d4ca-4f10-8f60-407a507edefe
     To      :  8791f25bd4ca4f108f60407a507edefe
+
+    :param uuid: uuid string with dashes
+    :type uuid: str
+
+    :returns: str - hex of uuid
     """
 
     return UUID(uuid).hex
