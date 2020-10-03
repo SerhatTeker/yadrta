cd6e6db250e35dc89f5a69aa89165a6975e22018
Serhat Teker <serhat.teker@gmail.com>
2020-10-03 17:10:35 +0300

    Update EnablePartialUpdateMixin doc

--- src/core/utils/views.py
+++ src/core/utils/views.py
@@ -1,7 +1,9 @@
 class EnablePartialUpdateMixin:
+    """Enable partial updates
+
+    Override partial kwargs in UpdateModelMixin class
+    https://github.com/encode/django-rest-framework/blob/91916a4db14cd6a06aca13fb9a46fc667f6c0682/rest_framework/mixins.py#L64
+    """
     def update(self, request, *args, **kwargs):
-        # Enable partial updates
-        # Override partial kwargs in UpdateModelMixin class
-        # https://github.com/encode/django-rest-framework/blob/91916a4db14cd6a06aca13fb9a46fc667f6c0682/rest_framework/mixins.py#L64
         kwargs['partial'] = True
         return super().update(request, *args, **kwargs)
