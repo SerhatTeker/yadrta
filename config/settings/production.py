db609dbbb6116130366bfab76cc827168aeaaa7d
Serhat Teker <serhat.teker@gmail.com>
2020-10-05 19:46:10 +0300

    Add gunicorn in INSTALLED_APPS

--- config/settings/production.py
+++ config/settings/production.py
@@ -9,6 +9,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", default=["example.com"])
 
+INSTALLED_APPS += ("gunicorn", )
+
 # DATABASES
 # ------------------------------------------------------------------------------
 DATABASES["default"] = env.db("DATABASE_URL")  # noqa F405
