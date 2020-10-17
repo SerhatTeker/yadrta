46675858383ac3f0334e9bb07f764ab560c2d6a2
Serhat Teker <serhat.teker@gmail.com>
2020-10-17 20:38:35 +0300

    Update postgres envs

--- wait_for_postgres.py
+++ wait_for_postgres.py
@@ -8,9 +8,9 @@ check_timeout = os.getenv("POSTGRES_CHECK_TIMEOUT", 30)
 check_interval = os.getenv("POSTGRES_CHECK_INTERVAL", 1)
 interval_unit = "second" if check_interval == 1 else "seconds"
 config = {
-    "dbname": os.getenv("POSTGRES_DB", "postgres"),
+    "dbname": os.getenv("POSTGRES_DB", "yadrta"),
     "user": os.getenv("POSTGRES_USER", "postgres"),
-    "password": os.getenv("POSTGRES_PASSWORD", ""),
+    "password": os.getenv("POSTGRES_PASSWORD", "debug"),
     "host": os.getenv("DATABASE_URL", "postgres"),
 }
 
